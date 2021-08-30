92b2579fa8c319a4d4b52bd54d22ffcfee0f0a9e
AltheusV <maos@ecomp.poli.br>
2021-08-30 10:34:40 -0300

    refactor: update json grammar

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+
 const fs = require('fs');
 const ohm = require('ohm-js');
 
-const contents = fs.readFileSync('json-gramatic.ohm');
+const contents = fs.readFileSync('gramatic.ohm');
 
 const myGrammar = ohm.grammar(contents);
 
 const semantic = myGrammar.createSemantics();
 
-const userInput = fs.readFileSync('./inputs.txt', 'utf8' , (err, data) => {
+const userInput = fs.readFileSync('./input.txt', 'utf8' , (err, data) => {
     if (err) {
       console.error(err)
       return
     }
     return data
 });
+
+
+const result = myGrammar.match(userInput);
+if (result.succeeded()) {
+  console.log('Entrada válida');
+  const tree = semantic(result);
+  console.log(tree)
+} else {
+  console.log(result.message);
+  console.log("Entrada inválida!");
+}
